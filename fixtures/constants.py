ef93270b784bf13bb048d67b0ed08eaf00f24b7f
Pavel <pavech5@gmail.com>
2022-05-22 23:49:49 +0300

    add readme and pre-commit

--- fixtures/constants.py
+++ fixtures/constants.py
@@ -3,10 +3,14 @@ class RegisterMessages:
     SIMILAR_PASS = "The password is too similar to the username."
     REGISTER = "Register new user"
     ALREADY_EXISTS = "A user with that username already exists."
-    ERROR_USERNAME = "Enter a valid username. This value may contain " \
-                     "only letters, numbers, and @/./+/-/_ characters."
+    ERROR_USERNAME = (
+        "Enter a valid username. This value may contain "
+        "only letters, numbers, and @/./+/-/_ characters."
+    )
     PASS_DIDNT_MATCH = "The two password fields didn’t match."
-    ERROR_SHORT_PASS = "This password is too short. It must contain at least 8 characters."
+    ERROR_SHORT_PASS = (
+        "This password is too short. It must contain at least 8 characters."
+    )
     ERROR_COMMON_PASS = "This password is too common."
     ERROR_NUMERIC_PASS = "This password is entirely numeric."
     ERROR_INVALID_EMAIL = "Enter a valid email address."
@@ -17,8 +21,10 @@ class LoginMessages:
     LOGIN = "Aleppo"
     PASSWORD = "qwerqwerqwerqwer"
     LOG_IN = "Log in"
-    ERROR_USERNAME_OR_PASS = f"Please enter a correct username and password." \
-                             f" Note that both fields may be case-sensitive."
+    ERROR_USERNAME_OR_PASS = (
+        "Please enter a correct username and password. "
+        "Note that both fields may be case-sensitive."
+    )
 
 
 class OrderMessages:
@@ -27,4 +33,8 @@ class OrderMessages:
     BASKETS_HEADER = "Your booking: Aleppo"
     CUSTOMER = "customer: Aleppo"
     YOUR_ORDER = "Ваши заказы, Aleppo"
-
+    INFO_HOTEL_IN_BASKET_COUNTRY = "Country:"
+    INFO_HOTEL_IN_BASKET_ACCOMMODATION = "Accommodation:"
+    INFO_HOTEL_IN_BASKET_PRICE = "Cost per day:"
+    INFO_HOTEL_IN_BASKET_COUNT_NIGHTS = "Number of nights:"
+    STATUS_ORDER = "формируется"
